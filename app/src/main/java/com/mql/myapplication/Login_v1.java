4f6344e93b310e606c8a945baf23e616307d3f06
Achraftraf <achrafzarouki20@gmail.com>
2023-11-06 01:12:25 +0100

    this the project with the dreable files and the xml file this is a ui of login page

---
+++ app/src/main/java/com/mql/myapplication/Login_v1.java
@@ -0,0 +1,14 @@
+package com.mql.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_v1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_v1);
+    }
+}
